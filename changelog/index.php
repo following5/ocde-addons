6e7dcbadeae868e5f20013db61450b3745d1ffed
following <following@online.de>
2017-09-24 00:37:18 +0200

    changelog design

--- changelog/index.php
+++ changelog/index.php
@@ -7,18 +7,20 @@
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
   <meta http-equiv="Content-Language" content="DE" />
   <link rel="stylesheet" type="text/css" href="https://www.opencaching.de/resource2/ocstyle/css/style_screen.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" /> 
 </head>
-<body>
+<body style="font-family:'Roboto',sans-serif">
 
 <style>
-  p { font-size:13px; line-height: 1.4em; padding-bottom:0.5em}
-  li { font-size:13px; padding-bottom:0.05em !important}
+  .content2-pagetitle { font-size:18px; }
+  p { font-family:'Roboto',sans-serif; font-size:15px; line-height: 1.4em; padding-bottom:0.5em; }
+  li { font-family:'Roboto',sans-serif; font-size:15px; padding-bottom:0.05em !important; }
   a { color:#b26c0a; text-decoration:none; }
   a:hover { color:red; }
 </style>
 
 <div style="width: 100%; background-color:white; padding-top:1em">
-<div style="margin:0 auto; width:840px">
+<div style="margin:0 auto; width:800px">
 <div style="text-align:right"><p>
   <big>Opencaching.de Changelog</big><br />
   <a href="https://github.com/OpencachingDeutschland/oc-server3/blob/development/LICENSE.md" target="_blank">license</a> &bull;
