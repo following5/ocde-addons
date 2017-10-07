681f5b2d6228f9943af9118eccd9359c39133cc3
following <following@online.de>
2017-10-07 21:57:02 +0200

    added full dbupdate.php

---
+++ tools/dbupdate.php
@@ -0,0 +1,62 @@
+#!/usr/bin/php
+<?php
+/***************************************************************************
+ * Original author: following
+ * license: MIT
+ ***************************************************************************/
+
+/*
+ * Update all code-version-dependent data on a developer system.
+ * This includes also doctrine migrations, which are missing in dbupdate.php
+ * in the OC repo.
+ */
+
+$rootpath = $opt['rootpath'] = __DIR__ . '/../../htdocs/';
+chdir($rootpath);
+require_once 'lib2/cli.inc.php';
+
+echo "updating composer dependencies\n";
+system('composer install');
+
+echo "applying sql deltas\n";
+require '../bin/dbsv-update.php';
+system('bin/console doctrine:migrations:migrate -n|sed "/^\s*$/d"|sed "/Application Migrations/d"');
+
+echo "importing static-data/data.sql\n";
+system(
+    'cat ' . $rootpath . '../sql/static-data/*.sql |' .
+    ' mysql -h' . $opt['db']['servername'] . ' -u' . $opt['db']['username'] . ' --password=' . $opt['db']['password'] . ' ' . $opt['db']['placeholder']['db']
+);
+
+echo "importing triggers\n";
+chdir('../sql/stored-proc');
+system('php maintain.php');
+
+// We do *two* tests for OKAPI presence to get some robustness agains internal OKAPI changes.
+//
+// This should be replaced by a facade function call, but current OKAPI implementation
+// does not work well when called from the command line, due to exception handling problems
+// (see http://code.google.com/p/opencaching-api/issues/detail?id=243).
+$okapi_vars = sql_table_exists('okapi_vars');
+$okapi_syncbase = sql_field_exists('caches', 'okapi_syncbase');
+if ($okapi_vars != $okapi_syncbase) {
+    echo "!! unknown OKAPI configuration; either dbupdate.php needs an update or your database configuration is wrong\n";
+} elseif ($okapi_vars) {
+    echo "updating OKAPI database\n";
+    chdir($rootpath . '../bin');
+    system('php okapi-update.php | grep -i -e mutation');
+}
+
+/*
+echo "importing translations\n";
+system('wget -O translations.zip "https://www.opencaching.de/translations.zip"');
+system('unzip -o translations.zip');
+system('rm translations.zip');
+foreach(['de', 'el', 'en', 'es', 'fr', 'it', 'nl', 'pl', 'ru'] as $lang)
+    system("bin/console translation:update $lang --force");
+system('bin/console translation:import-legacy-translation');
+*/
+
+echo "resetting webcache:\n";
+chdir($rootpath . '../bin');
+system('php clear-webcache.php');
